686a6b5ddccfb31dda6d0928d7521695dd7f83c8
Krina Soni <Krina.Soni@synechron.com>
2020-06-04 22:05:19 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LoginGuard } from './login.guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', canActivate: [LoginGuard], component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
